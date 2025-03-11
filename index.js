383daf9fc5e6a50c489d10810b81fc255bfcab61
aakash4372 <aakashaakash0013@gmail.com>
2025-03-11 12:59:43 +0530

    cors error

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  }));
+  
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
