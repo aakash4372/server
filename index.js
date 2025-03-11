519b4b132af6b79e1b3a0fdd502888fcc7297f7c
aakash4372 <aakashaakash0013@gmail.com>
2025-03-11 10:39:38 +0530

    index.js updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Education Server!');
+    res.send('Welcome to the Education Server!!');
 });
 
 //
